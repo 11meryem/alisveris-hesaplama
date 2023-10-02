3df579a58958687cae61c998483e4d46a6de3f39
11meryem <145452149+11meryem@users.noreply.github.com>
2023-10-02 14:24:37 +0300

    Add files via upload

---
+++ button.13/button.13/button.13/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace button._13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbltoplam = new System.Windows.Forms.Label();
+            this.txtbirim = new System.Windows.Forms.TextBox();
+            this.txtadet = new System.Windows.Forms.TextBox();
+            this.btnhesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Birim fiyat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Adet";
+            // 
+            // lbltoplam
+            // 
+            this.lbltoplam.AutoSize = true;
+            this.lbltoplam.Location = new System.Drawing.Point(110, 149);
+            this.lbltoplam.Name = "lbltoplam";
+            this.lbltoplam.Size = new System.Drawing.Size(70, 13);
+            this.lbltoplam.TabIndex = 2;
+            this.lbltoplam.Text = "Toplam Tutar";
+            // 
+            // txtbirim
+            // 
+            this.txtbirim.Location = new System.Drawing.Point(97, 62);
+            this.txtbirim.Name = "txtbirim";
+            this.txtbirim.Size = new System.Drawing.Size(100, 20);
+            this.txtbirim.TabIndex = 3;
+            // 
+            // txtadet
+            // 
+            this.txtadet.Location = new System.Drawing.Point(97, 88);
+            this.txtadet.Name = "txtadet";
+            this.txtadet.Size = new System.Drawing.Size(100, 20);
+            this.txtadet.TabIndex = 4;
+            // 
+            // btnhesapla
+            // 
+            this.btnhesapla.Location = new System.Drawing.Point(229, 79);
+            this.btnhesapla.Name = "btnhesapla";
+            this.btnhesapla.Size = new System.Drawing.Size(84, 61);
+            this.btnhesapla.TabIndex = 5;
+            this.btnhesapla.Text = "HESAPLA";
+            this.btnhesapla.UseVisualStyleBackColor = true;
+            this.btnhesapla.Click += new System.EventHandler(this.btnhesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 246);
+            this.Controls.Add(this.btnhesapla);
+            this.Controls.Add(this.txtadet);
+            this.Controls.Add(this.txtbirim);
+            this.Controls.Add(this.lbltoplam);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbltoplam;
+        private System.Windows.Forms.TextBox txtbirim;
+        private System.Windows.Forms.TextBox txtadet;
+        private System.Windows.Forms.Button btnhesapla;
+    }
+}
+
